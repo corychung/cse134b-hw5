ff9b33ed67578a4ff63d8fb7c7d8c08a3ddba56c
Cory Chung <cochung@ucsd.edu>
2025-11-25 14:01:39 -0800

    feat: add data loading for localStorage, JSONBin

--- project-card.js
+++ project-card.js
@@ -1,3 +1,5 @@
+// Part 1: project-card element
+
 class projectCard extends HTMLElement {
     
     constructor() {
@@ -79,3 +81,85 @@ class projectCard extends HTMLElement {
 }
 
 customElements.define("project-card", projectCard);
+
+// Part 2: Local and remote buttons
+
+const grid = document.getElementById("project-grid");
+const loadLocalBtn = document.getElementById("load-local");
+const loadRemoteBtn = document.getElementById("load-remote");
+
+const localProjects = [
+    {
+        title: "DIY 3D Printer",
+        desc: "Creating a fully featured CoreXY 3D printer completely from scratch.",
+        url: "projects/duender.html",
+        img: "/media/images/duender/duender.png",
+        img600: "/media/images/duender/duender-600.webp",
+        img1200: "/media/images/duender/duender-1200.webp",
+        img2000: "/media/images/duender/duender-2000.webp",
+        alt: "Screenshot of the Duender 3D printer"
+    },
+    {
+        title: "The Odin Project",
+        desc: "My experience working through The Odin Project's web dev curriculum.",
+        url: "projects/odin.html",
+        img: "/media/images/odin/odin.png",
+        img600: "/media/images/odin/odin-600.webp",
+        img1200: "/media/images/odin/odin-1200.webp",
+        img2000: "/media/images/odin/odin-2000.webp",
+        alt: "The Odin Project logo"
+    },
+    {                 
+        title:"This Portfolio Site!",
+        desc:"The very site you're on right now, built from scratch in HTML and CSS (no JS!)",
+        url:"projects/portfolio.html",
+        img:"/media/images/portfolio_thumbnail/portfolio_thumbnail.png",
+        img600:"/media/images/portfolio_thumbnail/portfolio_thumbnail-600.webp",
+        img1200:"/media/images/portfolio_thumbnail/portfolio_thumbnail-1200.webp",
+        img2000:"/media/images/portfolio_thumbnail/portfolio_thumbnail-2000.webp",
+        alt:"Picture of the code behind this portfolio site"
+    }
+];
+
+localStorage.setItem("projects", JSON.stringify(localProjects));
+
+function createCard(projectData) {
+    const card = document.createElement("project-card");
+    for (const attr in projectData) {
+        card.setAttribute(attr, projectData[attr]);
+    }
+    return card;
+}
+
+loadLocalBtn.addEventListener("click", () => {
+    grid.innerHTML = ""; // clear grid
+    const localData = JSON.parse(localStorage.getItem("projects")) || [];
+    localData.forEach(project => {
+        const card = createCard(project);
+        grid.appendChild(card);
+    });
+}); 
+
+const remoteUrl = "https://api.jsonbin.io/v3/b/69261bded0ea881f40001095";
+let projects = {};
+
+loadRemoteBtn.addEventListener("click", async () => {
+    grid.innerHTML = ""; // clear grid
+
+    try {
+        const response = await fetch(remoteUrl);
+        if (!response.ok) {
+            throw new Error(`${response.status}`);
+        }
+        const jsonData = await response.json();
+        const projects = jsonData.record || [];
+
+        projects.forEach(key => {
+            const card = createCard(key);
+            grid.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
