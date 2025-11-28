42e3e4c8108d1e50a8c5836c6a987bd0eb4c17ad
Cory Chung <cochung@ucsd.edu>
2025-11-28 12:52:20 -0800

    fix: ensure form field state reset on button press

--- crud.js
+++ crud.js
@@ -3,17 +3,19 @@ const desc = document.getElementById("desc");
 const url = document.getElementById("url");
 const img = document.getElementById("img");
 const imgAlt = document.getElementById("img-alt");
+
 const editButton = document.getElementById("edit-button");
 const createButton = document.getElementById("create-button");
 const deleteButton = document.getElementById("delete-button");
+
+const form = document.getElementById("crud-form");
 const projectDropdown = document.getElementById("project-select");
 const projectList = document.getElementById("project-list");
 const output = document.getElementById("crud-output");
-const form = document.getElementById("crud-form");
 
 let localProjects = JSON.parse(localStorage.getItem("localProjects")) || [];
 
-function refreshProjectList() {
+function reloadProjectList() {
     projectList.innerHTML = "";
     if (localProjects.length == 0 || !localProjects) {
         let option = document.createElement("option");     
@@ -31,8 +33,8 @@ function refreshProjectList() {
     }
 }
 
-// Handle auto-fill for project selection
-projectDropdown.addEventListener("change", () => {
+// Reload form field states based on dropdown selection and default projects
+function reloadFormFieldStates() {
     if (projectDropdown.value === "create") {
         form.reset();
         editButton.disabled = true;
@@ -56,24 +58,28 @@ projectDropdown.addEventListener("change", () => {
         createButton.disabled = true;
         createButton.style.backgroundColor = "gray";
 
-        if (localProjects[projectDropdown.value].default === "yes") {
+        if (project.default === "yes") {
             img.setAttribute("readonly", "true");   
             img.style.backgroundColor = "lightgray";
             url.setAttribute("readonly", "true");
             url.style.backgroundColor = "lightgray";
             output.value = "Note: Default projects have read-only image and URL fields.";
-            return;
+        } else {
+            img.removeAttribute("readonly");
+            img.style.backgroundColor = "";
+            url.removeAttribute("readonly");
+            url.style.backgroundColor = "";
+            output.value = "";
         }
     }
-    img.removeAttribute("readonly");
-    img.style.backgroundColor = "";
-    url.removeAttribute("readonly");
-    url.style.backgroundColor = "";
-    output.value = "";
+}
+
+projectDropdown.addEventListener("change", () => {
+    reloadFormFieldStates();
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    refreshProjectList();
+    reloadProjectList();
     editButton.disabled = true;
     editButton.style.backgroundColor = "gray";
     deleteButton.disabled = true;
@@ -97,21 +103,24 @@ form.addEventListener("submit", (event) => {
             alt: formData.get("img-alt"),
             default: "no"
         };
-        form.reset();
         localProjects.push(newProject);
+
         localStorage.setItem("localProjects", JSON.stringify(localProjects));
+        form.reset();
+        reloadFormFieldStates();
         output.value = "Project created successfully.";
         setTimeout(() => {output.value = "";}, 1500);
-        refreshProjectList();
+        reloadProjectList();
+
     } else if (action === "edit") {
         const project = localProjects[projectDropdown.value];
-        if (
-            project.title === formData.get("title") &&
+
+        // Check for no changes
+        if (project.title === formData.get("title") &&
             project.desc === formData.get("desc") &&
             project.url === formData.get("url") &&
             project.img === formData.get("img") &&
-            project.alt === formData.get("img-alt")
-        ) 
+            project.alt === formData.get("img-alt")) 
         {
             output.value = "No changes detected to save.";
             setTimeout(() => {output.value = "";}, 1500);
@@ -129,19 +138,21 @@ form.addEventListener("submit", (event) => {
         
         localStorage.setItem("localProjects", JSON.stringify(localProjects));
         form.reset();
+        reloadFormFieldStates();
         output.value = "Project edited successfully.";
         setTimeout(() => {output.value = "";}, 1500);
-        refreshProjectList();
+        reloadProjectList();
 
     } else if (action === "delete") {
         const index = projectDropdown.value;
-        localProjects.splice(index, 1);
-        localStorage.setItem("localProjects", JSON.stringify(localProjects));
 
+        localProjects.splice(index, 1);
+
+        localStorage.setItem("localProjects", JSON.stringify(localProjects));
         form.reset();
+        reloadFormFieldStates();
         output.value = "Project deleted successfully.";
         setTimeout(() => {output.value = "";}, 1500);
-
-        refreshProjectList();
+        reloadProjectList();
     }
 });
